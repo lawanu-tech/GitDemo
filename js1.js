5bc0287babe067f974a041c8cee21898ce2443e0
lawanu <lawanu1@gmail.com>
2022-08-16 22:30:34 +0530

    My fourth program

--- js1.js
+++ js1.js
@@ -4,3 +4,5 @@ console.log("My first js1 program");
 console.log("My second js2 program");
 /*my Third JS Program*/
 console.log("My third js3 program");
+/*my Fourth JS Program*/
+console.log("My fourth js4 program");
